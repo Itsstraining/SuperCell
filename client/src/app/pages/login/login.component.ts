62af3f90683facdc688227e5d88aee452e067162
Nguyen Tai Kiet <kiet.nt1494@sinhvien.hoasen.edu.vn>
2023-03-03 15:31:55 +0700

    create login page

---
+++ client/src/app/pages/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+// import { AngularFireAuth } from '@angular/fire/auth';
+// import * as firebase from 'firebase/app';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  // constructor(private auth: AngularFireAuth) { }
+
+  // loginWithGoogle() {
+  //   this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  // }
+
+  // logout() {
+  //   this.auth.signOut();
+  // }
+
+}
